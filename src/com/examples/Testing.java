fb20396a88ee4b5b8f68c65918784e1a445e98fd
Manikanta <maniiiiii0007@gmail.com>
2023-12-12 17:25:37 +0530

    added new repo

---
+++ src/com/examples/Testing.java
@@ -0,0 +1,18 @@
+package com.examples;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+public class Testing {
+	
+	
+	@Test
+	public void test1()
+	{
+		Example1 example1 = new Example1();
+		example1.Example(10, 10, "ADD");
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CalucaltionExample {
+	requires org.junit.jupiter.api;
+	requires junit;
+}
